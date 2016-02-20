d6a067b286c1255af44bd50af14079c3d3358f59
root <root@vader>
2016-02-20 20:44:52 +0100

    minor changes

--- js/quique.js
+++ js/quique.js
@@ -1,26 +1,31 @@
 
 
+$(function(){
+    $('#add-restriction-button').on('click',function(){
+
+        var r= $('<div class="form-group">'+
+                    '<label>Reglas</label>'+
+                    '<select class="form-control" id="listRules">'+
+                    '<option name="contain"> Contiene</option>'+                 
+                    '<option name="checkCopypaste">Verificar que no sea copiado de otras páginas</option>'+                           
+                    '<option name="spelling">Verificar ortografía</option>'+
+                    '<option name="replyTime">Controlar tiempo de respuesta</option>'+
+                    '<option name = "psicotecnico">Hacer antes psicotécnico</option>'+
+                   '</select>'+
+                  '</div> ');
+        $("body").append(r);
+    });
+});
 
 
 
-function addFields(){
-            // Number of inputs to create
-            var number = document.getElementById("member").value;
-            // Container <div> where dynamic content will be placed
-            var container = document.getElementById("container");
-            // Clear previous contents of the container
-            while (container.hasChildNodes()) {
-                container.removeChild(container.lastChild);
-            }
-            for (i=0;i<number;i++){
-                // Append a node with a random text
-                container.appendChild(document.createTextNode("Member " + (i+1)));
-                // Create an <input> element, set its type and name attributes
-                var input = document.createElement("input");
-                input.type = "text";
-                input.name = "member" + i;
-                container.appendChild(input);
-                // Append a line break 
-                container.appendChild(document.createElement("br"));
-            }
-        }
+
+
+$('select').on('change', function() {
+  alert( this.value );
+})
+
+
+
+
+    
