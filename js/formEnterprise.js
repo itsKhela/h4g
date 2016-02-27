c341284d3d9e703a78b71795591717f8af3971cc
Leo Bernal Bueno <leobernal91@gmail.com>
2016-02-27 10:42:13 +0100

    todo

--- js/formEnterprise.js
+++ js/formEnterprise.js
@@ -6,21 +6,21 @@ $(function(){
         var r= $('<div class="form-group">'+
                     '<label>Reglas</label>'+
                     '<select class="form-control" id="listRules">'+
-                    '<option value="contain"> Contiene</option>'+                 
-                    '<option value="checkCopypaste">Verificar que no sea copiado de otras páginas</option>'+                           
+                    '<option value="contain"> Contiene</option>'+
+                    '<option value="checkCopypaste">Verificar que no sea copiado de otras páginas</option>'+
                     '<option value="spelling">Verificar ortografía</option>'+
                     '<option value="replyTime">Controlar tiempo de respuesta</option>'+
                     '<option value= "psicotecnico">Hacer antes psicotécnico</option>'+
                    '</select>'+
-                  '<input id="addRule" type="button" value="Añade esta restricción">'+
+                  '<input id="addRule" class="btn btn-success" type="button" value="Añade esta restricción">'+
                   '</div> ');
 
-        $("body").append(r);
+        $('.prueba1').append(r);
 
         $('#add-restriction-button').remove();
-        
+
         switchHtml();
-       
+
 
     });
 });
@@ -29,13 +29,13 @@ $(function(){
 
 function selects(){
   $('#addRule').click(function(){
-                  
+
   var option = $( "#listRules option:selected" ).val()
 
   if ( option=='checkCopypaste') {
- 
+
     alert('funca');
- 
+
     }
   });
 }
@@ -43,32 +43,32 @@ function selects(){
 
 
 
-function switchHtml(){  
+function switchHtml(){
                  $('#addRule').click(function(){
-                  
-                  var option = $( "#listRules option:selected" ).val();
-                 
 
-          switch (option) { 
-                case 'contain': 
+                  var option = $( "#listRules option:selected" ).val();
+
+
+          switch (option) {
+                case 'contain':
                     createContain();
 
 
                     break;
-                case 'checkCopypaste': 
+                case 'checkCopypaste':
                    createCheck();
                    $('option[value=checkCopypaste]').remove();
 
                     break;
-                case 'spelling': 
+                case 'spelling':
                     createSepelling();
                     $('option[value=spelling]').remove();
-                    break;    
-                case 'replyTime': 
+                    break;
+                case 'replyTime':
                     createReplyTime();
                     $('option[value=replyTime]').remove();
                     break;
-                case 'psicotecnico': 
+                case 'psicotecnico':
                     createPsico();
                     $('option[value=psicotecnico]').remove();
                     break;
@@ -76,12 +76,12 @@ function switchHtml(){
                   alert('Nobody Wins!');
                   };
 
-          
 
 
-              
+
+
         });
-        }; 
+        };
 
 
 function createContain(){
@@ -91,7 +91,7 @@ function createContain(){
   cont++;
   var comp = '<label for = '+f+'> Debe de contener</label>'+
               '<input id='+idcontain+' class="form-control" placeholder="Mete tu palabra o expresión" >'+
-              '<input id="'+delet+'" type="button" value="Borrar">  <br for ="'+delet+'"/>'
+              '<input id="'+delet+'" class="btn btn-danger" type="button" value="Borrar">  <br for ="'+delet+'"/>'
 
 
     $('#rectrictions').append(comp);
@@ -113,7 +113,7 @@ function createCheck(){
     $('#rectrictions').append(comp);
     deleteParam('#'+check,f,delet,check);
 
-    
+
 
 
 };
@@ -133,7 +133,7 @@ function createSepelling(){
     $('#rectrictions').append(comp);
     deleteParam('#'+spelling,f,delet,spelling);
 
-    
+
 
 
 };
@@ -144,7 +144,7 @@ function createReplyTime(){
   var delet = 'replytime';
     var comp = '<label for = '+f+'> Tiempo máximo </label>'+
               '<input id='+reply+' class="form-control" placeholder="Enter text" >'+
-              '<input id="'+delet+'" type="button" value="Borrar">  <br for ="'+delet+'"/>'
+              '<input id="'+delet+'" class="btn btn-danger" type="button" value="Borrar">  <br for ="'+delet+'"/>'
 
 
     $('#rectrictions').append(comp);
@@ -167,7 +167,7 @@ function createPsico(){
     $('#rectrictions').append(comp);
     deleteParam('#'+psico,f,delet,psico);
 
-    
+
 
 
 };
@@ -180,8 +180,8 @@ $('#'+del).click(function(){
   $(id).remove();
   $('#'+del).remove();
 
-  $('br[for='+del+']').remove();   
-  checkin(ide);  
+  $('br[for='+del+']').remove();
+  checkin(ide);
 
 });};
 
@@ -189,21 +189,21 @@ $('#'+del).click(function(){
 function checkin(ide){
 
 switch (ide) {
-  case 'checkcopypaste': 
+  case 'checkcopypaste':
     $('#listRules').append('<option value="checkCopypaste">Verificar que no sea copiado de otras páginas</option>');
-  
+
   break;
-  case 'checkspelling': 
+  case 'checkspelling':
     $('#listRules').append('<option value="spelling">Verificar ortografía</option>');
-  
+
   break;
-  case 'checkreplytime': 
+  case 'checkreplytime':
     $('#listRules').append('<option value="replyTime">Controlar tiempo de respuesta</option>');
-  
+
   break;
-  case 'checkpsicotecnico': 
+  case 'checkpsicotecnico':
     $('#listRules').append('<option value= "psicotecnico">Hacer antes psicotécnico</option>');
-  
+
   break;
 
 
